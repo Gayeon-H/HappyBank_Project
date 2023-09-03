8aee0f87721c84a85ada528074addb304c140c0c
HGY <922g@naver.com>
2023-09-04 00:15:10 +0900

    feat : 회원가입 관련 기능

--- src/main/java/com/hgy/happybank/member/type/Role.java
+++ src/main/java/com/hgy/happybank/member/type/Role.java
@@ -3,9 +3,6 @@ package com.hgy.happybank.member.type;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-
 @RequiredArgsConstructor
 @Getter
 public enum Role {
