0965b7658f9ccb1f079524626dd4a6e84930238f
HGY <922g@naver.com>
2023-09-07 17:08:22 +0900

    feat : 예외처리를 위한 customException 및 Handler 구현

---
+++ src/main/java/com/hgy/happybank/exception/type/ErrorCode.java
@@ -0,0 +1,17 @@
+package com.hgy.happybank.exception.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum ErrorCode {
+    EMAIL_DUPLICATED(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
+    NICKNAME_DUPLICATED(HttpStatus.CONFLICT, "이미 존재하는 닉네임입니다."),
+    EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 이메일입니다."),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.");
+
+    private HttpStatus httpStatus;
+    private String message;
+}
