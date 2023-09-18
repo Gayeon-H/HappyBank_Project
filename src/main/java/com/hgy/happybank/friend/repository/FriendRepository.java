94882a63aa4503cf3158ed3f881426957a2c75ec
Gayeon <922g@naver.com>
2023-09-18 21:50:28 +0900

    feat : FriendRepository 메서드 수정 및 page 처리 수정

--- src/main/java/com/hgy/happybank/friend/repository/FriendRepository.java
+++ src/main/java/com/hgy/happybank/friend/repository/FriendRepository.java
@@ -2,7 +2,10 @@ package com.hgy.happybank.friend.repository;
 
 import com.hgy.happybank.friend.domain.Friend;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,10 +15,19 @@ public interface FriendRepository extends JpaRepository<Friend, Long> {
 
     boolean existsByToMemberIdAndFromMemberId(Long toMemberId, Long fromMemberId);
 
-    Page<Friend> findByToMemberIdOrFromMemberIdAndAreWeFriends(Long toMemberId, Long fromMemberId, boolean areWeFriends);
+    @Query("SELECT f FROM Friend f WHERE (f.toMemberId = :to OR f.fromMemberId = :from) " +
+            "AND f.areWeFriends = :friend")
+    Page<Friend> findByToMemberIdOrFromMemberIdAndAreWeFriends(@Param("to") Long toMemberId,
+                                                               @Param("from") Long fromMemberId,
+                                                               @Param("friend") boolean areWeFriends,
+                                                               Pageable pageable);
 
-    Page<Friend> findByToMemberIdAndAreWeFriends(Long memberId, boolean areWeFriends);
+    Page<Friend> findByToMemberIdAndAreWeFriends(Long memberId, boolean areWeFriends, Pageable pageable);
 
-    Optional<Friend> findByToMemberIdAndFromMemberIdAndAreWeFriends(Long toMemberId, Long fromMemberId, boolean areWeFriends);
+    Optional<Friend> findByToMemberIdAndFromMemberIdAndAreWeFriends(Long toMemberId, Long fromMemberId,
+                                                                    boolean areWeFriends);
+
+    boolean existsByToMemberIdAndFromMemberIdAndAreWeFriends(Long toMemberId, Long fromMemberId,
+                                                             boolean areWeFriends);
 
 }
--- src/main/java/com/hgy/happybank/friend/service/FriendService.java
+++ src/main/java/com/hgy/happybank/friend/service/FriendService.java
@@ -9,6 +9,7 @@ import com.hgy.happybank.member.domain.dto.MemberDTO;
 import com.hgy.happybank.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
@@ -46,7 +47,8 @@ public class FriendService {
         Member toMember = memberRepository.findByEmail(email)
                 .orElseThrow(() -> new BizException(ErrorCode.MEMBER_NOT_FOUND));
 
-        return friendRepository.findByToMemberIdAndAreWeFriends(toMember.getId(), false)
+        return friendRepository.findByToMemberIdAndAreWeFriends(toMember.getId(), false
+                        , PageRequest.of(0, 10))
                 .map(request -> {
                     Member member = memberRepository.findById(request.getFromMemberId())
                             .orElseThrow(() -> new BizException(ErrorCode.MEMBER_NOT_FOUND));
@@ -61,7 +63,8 @@ public class FriendService {
         Member toMember = memberRepository.findByEmail(email)
                 .orElseThrow(() -> new BizException(ErrorCode.MEMBER_NOT_FOUND));
 
-        Friend friend = friendRepository.findByToMemberIdAndFromMemberIdAndAreWeFriends(toMember.getId(), memberId, false)
+        Friend friend = friendRepository.findByToMemberIdAndFromMemberIdAndAreWeFriends(toMember.getId()
+                        , memberId, false)
                 .orElseThrow(() -> new BizException(ErrorCode.FRIEND_REQUEST_ERROR, "친구 요청이 존재하지 않습니다."));
 
         friend.setAreWeFriends(true);
@@ -73,7 +76,8 @@ public class FriendService {
                 .orElseThrow(() -> new BizException(ErrorCode.MEMBER_NOT_FOUND));
 
         return friendRepository
-                .findByToMemberIdOrFromMemberIdAndAreWeFriends(member.getId(), member.getId(), true)
+                .findByToMemberIdOrFromMemberIdAndAreWeFriends(member.getId(), member.getId()
+                        , true, PageRequest.of(0, 10))
                 .map(friend -> {
                     long friendId = Objects.equals(friend.getToMemberId(), member.getId()) ?
                             friend.getFromMemberId() : friend.getToMemberId();
